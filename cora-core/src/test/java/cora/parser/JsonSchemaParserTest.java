4ab7a57b896b9bb088fa996f51e4bc532b831e69
黄伟祥 <hwx@huangweixiangdeMacBook-Air.local>
2021-11-19 17:02:59 +0800

    Refactor test

--- cora-core/src/test/java/cora/parser/JsonSchemaParserTest.java
+++ cora-core/src/test/java/cora/parser/JsonSchemaParserTest.java
@@ -1,12 +1,16 @@
 package cora.parser;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import graphql.language.Definition;
 import graphql.language.ObjectTypeDefinition;
-import graphql.language.Type;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
 
+import javax.json.JsonObject;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,65 +19,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class JsonSchemaParserTest {
 
     JsonSchemaParser jsonSchemaParser;
-
-    // a normal schema with $ref
-    private final String schema1 = "{\n" +
-            "    \"type\": \"OBJECT\",\n" +
-            "    \"nodeType\": \"Bill\",\n" +
-            "    \"properties\": {\n" +
-            "      \"invoiceCompany\": {\n" +
-            "        \"type\": \"STRING\",\n" +
-            "        \"title\": \"公司名\"\n" +
-            "      },\n" +
-            "      \"saler\": {\n" +
-            "        \"$ref\": \"#/$defs/Saler\"\n" +
-            "      }\n" +
-            "    }\n" +
-            "  }";
-
-    // a schema without "nodeType"
-    private final String schema2 = "{\n" +
-            "    \"type\": \"OBJECT\",\n" +
-            "    \"properties\": {\n" +
-            "      \"invoiceCompany\": {\n" +
-            "        \"type\": \"STRING\",\n" +
-            "        \"title\": \"公司名\"\n" +
-            "      },\n" +
-            "      \"saler\": {\n" +
-            "        \"$ref\": \"#/$defs/Saler\"\n" +
-            "      }\n" +
-            "    }\n" +
-            "  }";
-
-    // a schema without "type"
-    String schema3 = "{\n" +
-            "    \"nodeType\": \"Bill\",\n" +
-            "    \"properties\": {\n" +
-            "      \"invoiceCompany\": {\n" +
-            "        \"type\": \"STRING\",\n" +
-            "        \"title\": \"公司名\"\n" +
-            "      },\n" +
-            "      \"saler\": {\n" +
-            "        \"$ref\": \"#/$defs/Saler\"\n" +
-            "      }\n" +
-            "    }\n" +
-            "  }";
-
-    // a schema without "properties"
-    private final String schema4 = "{\n" +
-            "    \"type\": \"OBJECT\",\n" +
-            "    \"nodeType\": \"Bill\"\n" +
-            "  }";
-
-    // a schema with syntax error
-    private final String schema5 = "{\n" +
-            "    \"type\": \"OBJECT\",\n" +
-            "    \"nodeType\": \"Bill\",\n" +
-            "    \"properties\": {\n";
+    List<String> schemas = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
         jsonSchemaParser = new JsonSchemaParser();
+
+        final String path = "classpath:test/schemafortest.json";
+        File file = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            file = ResourceUtils.getFile(path);
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
+                String readLine = null;
+                while ((readLine = br.readLine()) != null) {
+                    sb.append(readLine);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        JSONArray jsonArray = (JSONArray) JSON.parse(sb.toString());
+        for (Object jsonObject:jsonArray){
+            schemas.add(jsonObject.toString());
+        }
     }
 
     @AfterEach
@@ -81,35 +53,64 @@ class JsonSchemaParserTest {
     }
 
     @Test
-    void isValid() {
-        assertTrue(jsonSchemaParser.isValid(schema1));
-        assertTrue(jsonSchemaParser.isValid(schema2));
-        assertTrue(jsonSchemaParser.isValid(schema3));
-        assertTrue(jsonSchemaParser.isValid(schema4));
-        assertFalse(jsonSchemaParser.isValid(schema5));
+    void isNormalScemaValid(){
+        assertTrue(jsonSchemaParser.isValid(schemas.get(0)));
     }
 
     @Test
-    void parseSchema() {
-        List<Definition> definitions1 = jsonSchemaParser.parseSchema(schema1);
-        assertEquals(1, definitions1.size());
-        ObjectTypeDefinition definition1 = (ObjectTypeDefinition) definitions1.get(0);
-        String name1  = definition1.getName();
-        assertEquals("Bill", name1);
-        List children1 = definition1.getChildren();
-        assertEquals(2, children1.size());
+    void isScemaWithoutNodetypeValid(){
+        assertTrue(jsonSchemaParser.isValid(schemas.get(1)));
+    }
+
+    @Test
+    void isSchemaWithoutTypeValid(){
+        assertTrue(jsonSchemaParser.isValid(schemas.get(2)));
+    }
+
+    @Test
+    void isSchemaWithoutPropertyValid(){
+        assertTrue(jsonSchemaParser.isValid(schemas.get(3)));
+    }
+
+    @Test
+    void isSchemaWithSyntaxErrorValid(){
+        jsonSchemaParser.isValid(schemas.get(4).substring(0, 49));
+    }
+
+    @Test
+    void parseNormalSchema(){
+        List<Definition> definitions = jsonSchemaParser.parseSchema(schemas.get(0));
+        ObjectTypeDefinition definition = (ObjectTypeDefinition) definitions.get(0);
+        List children = definition.getChildren();
         List<String> childrenNames = new ArrayList<>();
-        definition1.getFieldDefinitions().forEach(fieldDefinition -> {
+        definition.getFieldDefinitions().forEach(fieldDefinition -> {
             childrenNames.add(fieldDefinition.getName());
         });
         assertArrayEquals(childrenNames.toArray(new String[0]), new String[]{"saler", "invoiceCompany"});
 
-        List<Definition> definitions2 = jsonSchemaParser.parseSchema(schema2);
-        assertEquals(1, definitions2.size());
-        ObjectTypeDefinition definition2 = (ObjectTypeDefinition) definitions2.get(0);
-        String name2  = definition2.getName();
-        assertEquals(null, name2);
-        List children2 = definition2.getChildren();
-        assertEquals(2, children2.size());
+    }
+
+    @Test
+    void parseSchemaWithoutNodetype() {
+        List<Definition> definitions = jsonSchemaParser.parseSchema(schemas.get(1));
+        ObjectTypeDefinition definition = (ObjectTypeDefinition) definitions.get(0);
+        String name = definition.getName();
+        assertEquals(null, name);
+    }
+
+    @Test
+    void parseSchemaWithouType(){
+        List<Definition> definitions = jsonSchemaParser.parseSchema(schemas.get(2));
+        ObjectTypeDefinition definition = (ObjectTypeDefinition) definitions.get(0);
+        String name  = definition.getName();
+        assertEquals("Bill", name);
+    }
+
+    @Test
+    void parseSchemaWithoutProperty() {
+        List<Definition> definitions = jsonSchemaParser.parseSchema(schemas.get(3));
+        ObjectTypeDefinition definition = (ObjectTypeDefinition) definitions.get(0);
+        List children = definition.getChildren();
+        assertEquals(0, children.size());
     }
 }
